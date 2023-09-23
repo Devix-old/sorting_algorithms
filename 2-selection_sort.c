fff3dd0e419b0514f3b30b2e86eddd9629cf6742
Devix-old <mohamed.elgharef@gmail.com>
2023-09-23 18:00:55 +0100

    Task 2 update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,27 +1,37 @@
 #include "sort.h"
 /**
+ * swap - swap two element in an array
+ * @xp: the fisrt element to swap
+ * @yp: the second element to swap
+ */
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+/**
  * selection_sort - this function sort an array using selection sort
  * @array: the array to sort
  * @size: the size of the array
  */
+
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, minIndex, temp;
+	size_t i, j, min_idx;
 
+	if (array == NULL || size < 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		minIndex = i;
+
+		min_idx = i;
 		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[minIndex])
-				minIndex = j;
-		}
-		if ((int)i != array[minIndex])
-		{
-			temp = array[i];
-			array[i] = array[minIndex];
-			array[minIndex] = temp;
-		}
+			if (array[j] < array[min_idx])
+				min_idx = j;
+
+		if (min_idx != i)
+			swap(&array[min_idx], &array[i]);
 		print_array(array, size);
 	}
 }
